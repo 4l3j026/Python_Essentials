713982b0e9fb2b5d0c2ffe29c28111dc55667956
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-05 18:54:54 -0500

    Create exercise about assignment operators.

---
+++ Assignment_Operators/AssignmentOperators.py
@@ -0,0 +1,11 @@
+#Assignment operators 
+A = 10
+print("The initial value of the A is: ", A)
+A += 1 
+print("The value after use += 1 is: ", A)
+A -= 2 
+print("The value after use -= 2 is: ", A)
+A *= 4
+print("The value after use *= 4 is: ", A)
+A /= 6
+print("The value after use /= 6 is: ", A)
