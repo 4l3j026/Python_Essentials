434f5448d39de8d5450f3ecec213e8424c2caeda
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-05 18:25:31 -0500

    Arithmetic operators exercise.

---
+++ Arithmetic_Operators/#Arithmetic Operators.py
@@ -0,0 +1,18 @@
+#Arithmetic Operators 
+A = 18
+B = 32
+C = 2
+Result = A + B
+Result_Substraction = A - B
+Product = A * C
+Divide = B / C
+Integer_Division = 10 // 3
+Normal_Division = 10 / 3
+Mod_Division = 10 % 3
+print("The value of A + B is: ", Result)
+print("The value of A - B is: ", Result_Substraction)
+print("The value of A * C is: ", Product)
+print("The value of B / C is: ", Divide)
+print("The integer value between 10 / 3 is: ", Integer_Division)
+print("The value of 10 / 3 is: ", Normal_Division)
+print("The value of 10 % 3 is: ", Mod_Division)
