5e0f61b18643489b586803d5d9ec9abfa85551b6
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-05 19:50:57 -0500

    Update files of the project.

--- Comparision_Operators/ComparisionOperators.py
+++ Comparision_Operators/ComparisionOperators.py
@@ -11,3 +11,6 @@ print("The result of 4 < 2 is: ", 4 < 2)
 print("The result of 8 > 5 is: ", 8 > 5)
 print("The result of 4 <= 4: \n", 4 <= 4)
 print("The result of 12 >= 6 is: \n", 12 >= 6)
+
+switch 3 < 4
+    print("The result of 12 >= 6 is: \n", 12 >= 6)
