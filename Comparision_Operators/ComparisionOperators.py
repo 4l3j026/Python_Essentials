67097a2580c184c6f144259bc0ea62d89ca92cf2
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-05 19:11:57 -0500

    Create comparision exercise.

---
+++ Comparision_Operators/ComparisionOperators.py
@@ -0,0 +1,10 @@
+#Comparision Operators Exercise
+A = 10
+B = 8 
+Result = A == B
+print("Result between comparision 10 == 8: ", Result)
+print("The result between comparision 9 != 780 is: ", 9!= 780)
+print("The result of 4 < 2 is: ", 4 < 2)
+print("The result of 8 > 5 is: ", 8 > 5)
+print("The result of 4 <= 4", 4 <= 4)
+print("The result of 12 >= 6", 12 >= 6)
