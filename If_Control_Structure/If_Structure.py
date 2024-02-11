71b69b009e50e3d9c5253245b3a9e1273060f30a
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-11 01:10:22 -0500

    Update files of the project

--- If_Control_Structure/If_Structure.py
+++ If_Control_Structure/If_Structure.py
@@ -3,8 +3,8 @@
     Create Multi-Lines comments 
     
 """
-Number = int(input("Enter integer value: "))
+Number = int(input("Enter integer value: ")) #Data input save as integer. 
 
-if Number > 0: 
+if Number > 0: #Check condition 
     print(f"The value {Number} is greater than 0")
     
