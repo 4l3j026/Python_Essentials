2366c8c419dcccd6e312ba30cf09dd1352efabbb
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-11 01:18:29 -0500

    Create an if and else exercise

---
+++ If_Else_Control_Structure/If_Else.py
@@ -0,0 +1,7 @@
+#Evaluate numbers
+Number_Input = int(input("Enter an integer number: "))
+
+if Number_Input < 0:
+    print("The number entered is negative.")
+else: 
+    print("The number entered is positive.")
