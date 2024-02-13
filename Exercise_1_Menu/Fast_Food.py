6e03d203a63bd7d8afd5d66b6fc9e2063a25befa
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-12 23:51:36 -0500

    Show to user the menu and create while condition

--- Exercise_1_Menu/Fast_Food.py
+++ Exercise_1_Menu/Fast_Food.py
@@ -1,2 +1,20 @@
-# Fast food Resturant exercise 
+# Fast food Resturant exercise using dictionary 
 print("Hello everyone and welcome to Prehistoric Fast Food!")
+#Show the user menu
+print("""
+      
+    1.  Triceratops Burguer =  $10.80 
+    2.  Brontosaurus Hot Dog = $18.60
+    3.  Velociraptor Sandwich = $8.00
+    4.  Tyrannosaurus Fries = $20.50
+    5.  Stegosaurus Pizza = $30.40
+      
+      """)
+
+Option = input("Would you like to order? (Y/N): ")
+ 
+while Option == 'Y' or Option == 'y':
+        
+    user_option = int(input("Select your option, entering a integer number of menu: ")) #Take input data
+
+
