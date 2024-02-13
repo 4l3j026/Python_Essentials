2b18f65d7acaafc8eca244eab28e1b1a0ceb081e
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-12 20:02:48 -0500

    Create exercise using elif estructure

---
+++ If_elif_else_Structure/If_elif_else.py
@@ -0,0 +1,12 @@
+#Test using if elif else
+
+Number = int(input("Enter a random integer number: "))
+
+if Number < 0: 
+    print("The number entered is negative!")
+elif Number > 0:
+    print("The number entered is positive!") 
+else: 
+    print("The number entered is 0! ")
+    
+
