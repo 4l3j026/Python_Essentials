7ecf508fbfea1f90e301bf4a838883a70cfacf2c
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-19 01:55:29 -0500

    Create file exercise

---
+++ Prime_Number/Integer_Prime_Number.py
@@ -0,0 +1,8 @@
+#Determining an integer number is positive and prime
+
+print("\n Welcome to this program!")
+
+number = int(input("Enter a number: "))
+
+if number <= 1: 
+    print("The number entered is not prime.")
