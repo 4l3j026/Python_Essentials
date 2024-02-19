a5963196ba682b600d1734ac2e84afef86092d4d
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-18 23:39:31 -0500

    Create triangle equilateral exercise

---
+++ Triangle_Area/Triangle_Equilateral_Area.py
@@ -0,0 +1,10 @@
+#Find the triangle equilateral area 
+
+print("Welcome to this program! \n\n Enter varaibles to calculate the Triangle Equilateral Area: \n")
+
+Base = float(input("Enter Base (m): "))
+Height = float(input("Enter Height (m): "))
+
+Area = ((Base * Height) / 2)
+
+print("The are of the triangle equilateral is: {:.2f} m^2".format(Area))
