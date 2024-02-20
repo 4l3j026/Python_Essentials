804f6134b83ed8211faab14794d7f95723219ce8
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-19 21:52:23 -0500

    Create prime number exercise

--- Prime_Number/Integer_Prime_Number.py
+++ Prime_Number/Integer_Prime_Number.py
@@ -4,5 +4,18 @@ print("\n Welcome to this program!")
 
 number = int(input("Enter a number: "))
 
-if number <= 1: 
+if number <= 1: #Number 0 and 1 is not prime number
+    
     print("The number entered is not prime.")
+    
+elif number == 2:
+    print("The number entered is a prime:") #The number 2 is the n
+    
+else: 
+    for i in range(2, number):
+        if number % i == 0:
+            print('The number entered is not prime.')
+            break
+        else:
+            print("The number entered is prime.")
+            break
