9166dbd4f94061d891f8d05bc473262d53442809
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-20 02:05:20 -0500

    Finish try and except example

--- Try_Except/try_except.py
+++ Try_Except/try_except.py
@@ -2,10 +2,32 @@
 
 print("Welcome to this progrma user!")
 
-dividend = int(input("Enter the number to divide: "))
+try: 
+    
+  dividend = int(input("Enter the number to divide: "))
 
-divisor = int(input("Enter the divisor: "))
+  divisor = int(input("Enter the divisor: "))
+  
+  result = dividend / divisor
+  
+except ZeroDivisionError as a: 
+    
+    print(a) 
+    print("The division can't resolve if divisor is 0")
+   
+except ValueError as a: 
+    
+    print(a)
+    print("Wrong value")
 
-result = dividend / divisor
-
-print(result)
+except Exception as a: #This block is not a good practice 
+    
+    print(a)
+    print("Something went wrong!") 
+    
+else:
+    print(result)
+    
+finally: 
+    
+    print("Remember always keep learn!!!")
