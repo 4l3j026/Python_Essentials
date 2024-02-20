98e6c0a4c136be6657fdc6273e9371aa393e1ba7
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-19 23:13:37 -0500

    Create new exercise

---
+++ Methods_Exercise/Methods_Exercise.py
@@ -0,0 +1,17 @@
+#Methods exercise 
+
+my_str = "Alejandro is a Musician and Engineer"
+
+my_str_upper = my_str.upper() #Convert string data to uppercase 
+
+print(my_str_upper)
+
+my_str_lower = my_str_upper.lower() #Convert string data to lowercase
+
+print(my_str_lower)
+
+my_str_replace = my_str_lower.replace('a', '4') #Replace character a instead of 4
+
+print(my_str_replace)
+
+print(my_str_replace.isdigit()) #This function check if string is a number digit, and return condition 
