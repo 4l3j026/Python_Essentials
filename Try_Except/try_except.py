27d09eedca76361d4123e193ff8ac248a24e8bc1
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-20 01:49:27 -0500

    Create first part of the exercise

---
+++ Try_Except/try_except.py
@@ -0,0 +1,11 @@
+#Try Except exercise 
+
+print("Welcome to this progrma user!")
+
+dividend = int(input("Enter the number to divide: "))
+
+divisor = int(input("Enter the divisor: "))
+
+result = dividend / divisor
+
+print(result)
