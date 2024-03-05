7a1d7f0a013d61d7d969780fc9a1820a9b6cdd40
Alejandro Reyes Leon <alle262001@gmail.com>
2024-03-05 16:10:56 -0500

    Create exercise data using matrix

---
+++ Matrix_Exercise/Matrix.py
@@ -0,0 +1,14 @@
+# Matriz Exercise
+
+m = [  # Create matrix 4x3
+    [3, 3, 10],
+    [0, 1, 5],
+    [8, 10, 234],
+    ["hello", "Metal", "Music"],
+]
+
+print(m)  # Show data inside the matrix
+
+print(type(m))  # Show data type of the matrix
+
+print(m[3][2])  # Show specific value of the matrix
