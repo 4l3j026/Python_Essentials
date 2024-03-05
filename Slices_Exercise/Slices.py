852eb507acb36d024699f2962c17f171d9fbb999
Alejandro Reyes Leon <alle262001@gmail.com>
2024-03-04 22:33:19 -0500

    Create slices exercise

---
+++ Slices_Exercise/Slices.py
@@ -0,0 +1,18 @@
+# Using slices
+
+list_1 = ["Mechatronics Engineering"]  # Create a list only one element
+
+print(len(list_1))  # Show size of the list 1
+
+list_2 = list("Mechatronics Engineering")  # Create second list with more elements
+
+print(len(list_2))  # Show size of the list 2
+
+list_3 = list("Cello")
+
+# You can write slices without values (initial value of the list: final value of the list)
+print(list_3[:])
+
+print(list_3[1:4])
+
+print(list_3[0:5:2])
