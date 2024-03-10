f920d02a138e888ce37580fde75d9fb456aadcfd
Alejandro Reyes Leon <alle262001@gmail.com>
2024-03-10 16:42:05 -0500

    Finish  dictionary exercise

--- Dictionary_Exercise.py
+++ Dictionary_Exercise.py
@@ -5,22 +5,50 @@ Tic Generation Dictionary Exercise Fast Food
 
 def main():
 
+    # Variables
+    Total_Order = 0.0  # Initialize value of the order
+
     # Create dictionary
     fast_food_menu = {
-        "Hot Dog": 5.00,
-        "Hamburger": 8.50,
-        "Big Sausage and Fries": 12.80,
-        "French Fries": 7.20,
-        "Soda": 2.80,
-        "Ice Cream": 4.30,
+        "hot dog": 5.00,
+        "hamburger": 8.50,
+        "big sausage and fries": 12.80,
+        "french fries": 7.20,
+        "soda": 2.80,
+        "ice cream": 4.30,
     }
 
     print("\n\n     Welcome to this Fast Food Restaurant! \n\n")
 
     print("Food Menu:")
 
-    for Key_Word, Value in fast_food_menu.items():
-        print(f"-> {Key_Word}, Price: {Value}")
+    for (
+        Key_Word,
+        Value,
+    ) in fast_food_menu.items():  # Print each tuples of the dictionary
+
+        print(f" -> {Key_Word}, Price: {Value}")
+
+    while True:
+
+        Product = input("\n  Enter an option: ")
+        Product = Product.lower()
+        Product = Product.strip()
+
+        if Product in fast_food_menu:
+
+            Total_Order += fast_food_menu[Product]
+            print(f"Product value: ${fast_food_menu[Product]}")
+
+        elif Product == "exit":
+
+            print(f"Your total order is: ${Total_Order:.2f}")
+
+            break
+
+        else:
+
+            print("The entered value is not valid!")
 
 
 if __name__ == "__main__":
