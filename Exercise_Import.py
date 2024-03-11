643f9baac551a95c0aec511656e52f7f15a455ad
Alejandro Reyes Leon <alle262001@gmail.com>
2024-03-10 22:47:30 -0500

    Create modularity exercises

---
+++ Exercise_Import.py
@@ -0,0 +1,6 @@
+# Exercise import files
+
+
+def Sum(x: int, y: int) -> int:
+
+    return x + y
---
+++ Exercise_import_2.py
@@ -0,0 +1,7 @@
+# Exercise import part 2
+
+import Exercise_Import
+
+test = Exercise_Import.Sum(8, 16)
+
+print(test)
