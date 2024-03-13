419fd97185dbb30ea0ec7e49b9d2231ca1ea4ee1
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-03-12 20:47:15 -0500

    Create key words of the dictionary

---
+++ Task_1_Dictionary.py
@@ -0,0 +1,31 @@
+#Dictionary Task
+
+def main ():
+    
+    print("Test")
+    a = input("Enter Data: ")
+    f = Input_Text(a)
+    #print(f)
+    
+    
+    
+    
+def Input_Text (User_Text: str): 
+    
+    text_lower = User_Text.lower()
+    text_comas = text_lower.split()
+    key_words = ()
+    key_words = text_comas
+    print(key_words)
+    
+    Final_Dictionary = {}
+    
+    Final_Dictionary = User_Text
+
+    return Final_Dictionary
+
+
+if __name__ == "__main__":
+    main()
+    
+    
